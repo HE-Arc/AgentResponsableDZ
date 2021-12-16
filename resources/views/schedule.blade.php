4fbe6dc5ab8f1b62c5e642872e9015652040b41c
Lenny Boegli <lenny.boegli@he-arc.ch>
2021-12-16 14:02:25 +0100

    Added the join button. Not tested since user is always unset without login

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,26 +1,45 @@
 @extends("layouts.app")
 @section("content")
 <table>
-    @foreach ($flights as $f)
+    {{ strtotime(Date::now()) }}
     <tr>
         <td>Avion</td>
         <td>Départ</td>
         <td>Booking</td>
-        <td>n° {{ $f->id }}
+        <td>Avion n°</td>
+        @if (isset($user))
+            <td>Rejoindre</td>
+        @endif
     </tr>
+    @foreach ($flights as $f)
+
     <tr>
         <td>
-        @if (strtotime($f->departure) < strtotime(Date::now()))
-            Departed
+
+        @if (strtotime($f->departure) < strtotime(Date::now()) /*If the flight already departed*/)
+            <img src="/images/schedule/landed.png"/>
         @else
-            Not Departed
+            <img src="/images/schedule/loadingPlane.png"/>
         @endif
         </td>
         <td>{{ date("H:m",strtotime($f->departure)) }}</td>
         <td>{{ count($f->users) }}/{{ $f->plane->seat_count}}</td>
         <td>Booking</td>
+        @if (!isset($user))
+            @if(strtotime($f->departure) < strtotime(Date::now()) /*If the flight already departed*/)
+                <img src="/images/schdule/joinPlaneDeparted.png"/>
+            @elseif (count($f->users) >= $f->plane->seat_count /*If the flight is full*/)
+                <img src="/images/schdule/joinPlaneFull.png"/>
+            @elseif($user->credits4000 <= 0 && $user->credits1500 <= 0  /*If there is space but the user is poor*/)
+                <img src="/images/schdule/joinPlaneNoCredits.png"/>
+            @else
+                <img src="/images/schdule/joinPlaneOK.png"/>
+            @endif
+            <td>Rejoindre</td>
+        @endif
     </tr>
     @endforeach
+
 </table>
 @endsection
 
