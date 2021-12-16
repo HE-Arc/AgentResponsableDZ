f401df01710827b4d6c65791b5070a9be2d75b8b
Jarod Girardin <jarod.girardin@he-arc.ch>
2021-12-16 17:01:41 +0100

    start adding AuthController
    Co-authored-by: LennyBo <LennyBo@users.noreply.github.com>

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,5 +2,6 @@
     <img id="imgWave" src="/images/header_image.png"/>
     <img id="imgLogo" src="/images/logo.png">
 
+    <a href="{{ route('auth.login') }}">se connecter</a>
     <a href="{{ route('user.create') }}">crée un compte</a>
 </header>
