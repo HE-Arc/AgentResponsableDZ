4e919d1e6416f34e8de56d1b1d4a9846da43da04
Jarod Girardin <jarod.girardin@he-arc.ch>
2022-01-12 08:39:14 +0100

    better ui

--- resources/views/editUser.blade.php
+++ resources/views/editUser.blade.php
@@ -4,75 +4,79 @@
 
 <h3>Modification de l'utilisateur:</h3>
     <div class="row">
-        <form action="{{ route('user.update', auth()->user()->id) }}" method="post">
-            @csrf
-            @method('put')
+        <div class="col">
+            <form action="{{ route('user.update', auth()->user()->id) }}" method="post">
+                @csrf
+                @method('put')
 
-            <div class="row">
-                <label for="name">Nouveau nom: </label>
-                <input type="text" name="name" value="{{ auth()->user()->name }}">
-                @error('name')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="row">
-                <label for="surname">Nouveau surnom: </label>
-                <input type="text" name="surname" value="{{ auth()->user()->surname }}">
-                @error('surname')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="row">
-                <label for="phone_number">Nouveau numéro de téléphone: </label>
-                <input type="phone" name="phone_number" value="{{ auth()->user()->phone_number }}">
-                @error('phone_number')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="row">
-                <input type="submit" value="valider"/>
-            </div>
-        </form>
-    </div>
-    <div class="row">
-        <form method="post" action="{{ route('user.update_email', auth()->user()->id) }}">
-            @csrf
-            @method("put")
-            <div class="row">
-                <label for="email">Nouveau mail: </label>
-                <input type="mail" name="email" value="{{ auth()->user()->email }}">
-            </div>
-            <div class="row">
-                <label for="email_confirmation">Confirmer mail: </label>
-                <input type="mail" name="email_confirmation" value="{{ auth()->user()->email }}">
-                @error('email')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="row">
-                <input type="submit" value="valider"/>
-            </div>
-        </form>
-    </div>
-    <div class="row">
-        <form method="post" action="{{ route('user.update_password', auth()->user()->id) }}">
-            @csrf
-            @method('put')
+                <div class="row">
+                    <label for="name">Nouveau nom: </label>
+                    <input type="text" name="name" value="{{ auth()->user()->name }}">
+                    @error('name')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="row">
+                    <label for="surname">Nouveau surnom: </label>
+                    <input type="text" name="surname" value="{{ auth()->user()->surname }}">
+                    @error('surname')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="row">
+                    <label for="phone_number">Nouveau numéro de téléphone: </label>
+                    <input type="phone" name="phone_number" value="{{ auth()->user()->phone_number }}">
+                    @error('phone_number')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="row">
+                    <input type="submit" value="valider"/>
+                </div>
+            </form>
+        </div>
+        <div class="col-1"></div>
+        <div class="col">
+            <form method="post" action="{{ route('user.update_email', auth()->user()->id) }}">
+                @csrf
+                @method("put")
+                <div class="row">
+                    <label for="email">Nouveau mail: </label>
+                    <input type="mail" name="email" value="{{ auth()->user()->email }}">
+                </div>
+                <div class="row">
+                    <label for="email_confirmation">Confirmer mail: </label>
+                    <input type="mail" name="email_confirmation" value="{{ auth()->user()->email }}">
+                    @error('email')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="row">
+                    <input type="submit" value="valider"/>
+                </div>
+            </form>
+        </div>
+        <div class="col-1"></div>
+        <div class="col">
+            <form method="post" action="{{ route('user.update_password', auth()->user()->id) }}">
+                @csrf
+                @method('put')
 
-            <div class="row">
-                <label for="password">Nouveau mot de passe: </label>
-                <input type="password" name="password"></input>
-            </div>
-            <div class="row">
-                <label for="password_confirmation">Confirmer nouveau mot de passe: </label>
-                <input type="password" name="password_confirmation"></input>
-                @error('password')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="row">
-                <input type="submit" value="valider"/>
-            </div>
-        </form>
+                <div class="row">
+                    <label for="password">Nouveau mot de passe: </label>
+                    <input type="password" name="password"></input>
+                </div>
+                <div class="row">
+                    <label for="password_confirmation">Confirmer nouveau mot de passe: </label>
+                    <input type="password" name="password_confirmation"></input>
+                    @error('password')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="row">
+                    <input type="submit" value="valider"/>
+                </div>
+            </form>
+        <div class="col-1"></div>
     </div>
 @endsection
--- resources/views/manage_users.blade.php
+++ resources/views/manage_users.blade.php
@@ -3,170 +3,181 @@
 @section("content")
 
 <div class="row">
-    <h3>Modifier les utilisateurs:</h3>
+    <div class="col">
+        <h3>Modifier les utilisateurs:</h3>
+    </div>
+    <div class="col-2"></div>
 </div>
 
 <div class="row">
-    <label for="user">Utilisateur à modifier :</label>
+    <div class="col">
+        <label for="user">Utilisateur à modifier :</label>
+        <select id="users" onChange="userChanged();">
+            @foreach ($users as $u)
+            <option value="{{$u->email}}">{{$u->name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="col-2"></div>
+</div>
+<div class="row">
+    <div class="col">
 
-    <select id="users" onChange="userChanged();">
-        @foreach ($users as $u)
-        <option value="{{$u->email}}">{{$u->name}}</option>
-        @endforeach
-    </select>
+        <form method="post" action="{{ route('user.update_from_admin', auth()->user()->id)}}">
+            @csrf
+            @method("put")
 
-    <form method="post" action="{{ route('user.update_from_admin', auth()->user()->id)}}">
-        @csrf
-        @method("put")
+            <input type="hidden" name="id" id="user_to_change1"></input>
 
-        <input type="hidden" name="id" id="user_to_change1"></input>
+            <label for="name">Nom</label>
 
-        <label for="name">Nom</label>
+            <div class="row">
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value="{{ old('name') }}"
+                    required
+                    />
+                @error('name')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
 
-        <div class="row">
-            <input
-                type="text"
-                id="name"
-                name="name"
-                value="{{ old('name') }}"
-                required
+            <div class="row">
+
+                <label for="surname">Surnom</label>
+
+                <input
+                    type="text"
+                    id="surname"
+                    name="surname",
+                    value="{{ old('surname') }}"
+                    required
+                    />
+                @error('surname')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="row">
+
+                <label for="phone_number">numéro de téléphone</label>
+
+                <input
+                    type="phone"
+                    id="phone_number"
+                    name="phone_number"
+                    value="{{ old('phone_number') }}"
+                    required
+                    />
+                @error('phone_number')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="row">
+
+                <label for="email">email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value="{{ old('email') }}"
+                    required
+                    />
+                @error('email')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="row">
+            </div>
+            <div class="row">
+
+                <label for="isRDZ">Est un responsable de DZ</label>
+                <input
+                type="checkbox"
+                id="isRDZ"
+                name="isRDZ"
                 />
-            @error('name')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
+                @error('isRDZ')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
 
-        <div class="row">
+            <div class="row">
 
-            <label for="surname">Surnom</label>
+                <label for="credits4000">Crédits sauts a 4000m</label>
 
-            <input
-                type="text"
-                id="surname"
-                name="surname",
-                value="{{ old('surname') }}"
-                required
-                />
-            @error('surname')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
+                <input
+                type="number"
+                id="credits4000"
+                name="credits4000"
+                value="{{ old('credits4000') }}"
+                required />
+                @error('credits4000')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
 
-        <div class="row">
+            </div>
 
-            <label for="phone_number">numéro de téléphone</label>
+            <div class="row">
 
-            <input
-                type="phone"
-                id="phone_number"
-                name="phone_number"
-                value="{{ old('phone_number') }}"
-                required
-                />
-            @error('phone_number')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
+                <label for="credits1500">Crédits sauts a 1500m</label>
+                <input
+                type="number"
+                id="credits1500"
+                name="credits1500"
+                value="{{ old('credits4000') }}"
+                required />
+                @error('credits1500')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
 
-        <div class="row">
+            <div class="row">
+                <input type="submit" value="valider"></input>
+            </div>
+        </form>
+    </div>
+    <div class="col-1"></div>
+    <div class="col">
+        <form method="post" action="{{ route('user.update_password_from_admin', auth()->user()->id)}}">
 
-            <label for="email">email</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                value="{{ old('email') }}"
-                required
-                />
-            @error('email')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
+            <input type="hidden" name="id" id="user_to_change2"></input>
 
-        <div class="row">
-        </div>
-        <div class="row">
+            @csrf
+            @method("put")
+            <div class="row">
+                <label for="password">mot de passe</label>
+                <input
+                    type="password"
+                    name="password"
+                    required
+                    />
+                @error('password')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
 
-            <label for="isRDZ">Est un responsable de DZ</label>
-            <input
-            type="checkbox"
-            id="isRDZ"
-            name="isRDZ"
-             />
-            @error('isRDZ')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
+            <div class="row">
+                <label for="password_confirmation">confirmer mot de passe</label>
+                <input
+                    type="password"
+                    name="password_confirmation"
+                    required
+                    />
+                @error('confirm_password')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
 
-        <div class="row">
-
-            <label for="credits4000">Crédits sauts a 4000m</label>
-
-            <input
-            type="number"
-            id="credits4000"
-            name="credits4000"
-            value="{{ old('credits4000') }}"
-            required />
-            @error('credits4000')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-
-        </div>
-
-        <div class="row">
-
-            <label for="credits1500">Crédits sauts a 1500m</label>
-            <input
-            type="number"
-            id="credits1500"
-            name="credits1500"
-            value="{{ old('credits4000') }}"
-            required />
-            @error('credits1500')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div class="row">
-            <input type="submit" value="valider"></input>
-        </div>
-    </form>
-
-    <form method="post" action="{{ route('user.update_password_from_admin', auth()->user()->id)}}">
-
-        <input type="hidden" name="id" id="user_to_change2"></input>
-
-        @csrf
-        @method("put")
-        <div class="row">
-            <label for="password">mot de passe</label>
-            <input
-                type="password"
-                name="password"
-                required
-                />
-            @error('password')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div class="row">
-            <label for="password_confirmation">confirmer mot de passe</label>
-            <input
-                type="password"
-                name="password_confirmation"
-                required
-                />
-            @error('confirm_password')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div class="row">
-            <input type="submit" value="valider"></input>
-        </div>
-    </form>
+            <div class="row">
+                <input type="submit" value="valider"></input>
+            </div>
+        </form>
+    </div>
 </div>
 
 @push('scripts')
